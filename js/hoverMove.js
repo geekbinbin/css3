5b2736d659e6b72f774d2353a52a55ec407399a1
wangbinbin <wangbb3307586@qq.com>
2017-03-22 10:05:43 +0800

    增加鼠标移入动态移动line

---
+++ js/hoverMove.js
@@ -0,0 +1,11 @@
+/**
+ * Created by wangbinbin on 2017/3/21.
+ */
+'use strict';
+$(function () {
+    $('span').hover(function (e) {
+          console.log($(e));
+    },function () {
+
+    })
+});
